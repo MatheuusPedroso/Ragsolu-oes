06785ebfed8e7674c5a80dbd02a3b455ebe39b87
Matheus Pedroso <matheus.dsilvapedroso@outlook.com>
2025-04-24 15:59:31 -0300

    Configura build para pasta docs e adiciona

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ragsolu-oes/', 
+  base: '/Ragsolu-oes/',
+  build: {
+    outDir: 'docs' 
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
